523f498a12adec5249784697ec48e8cfee9270f4
Matthew McEneaney <mfm45@phyadmins-MBP.wired.duke.local>
2023-09-07 11:45:14 -0400

    Simplified input structure to just dictionary instead of list of dictionaries with extraneous entries in `orchestrate.py`.

--- orchestrate.py
+++ orchestrate.py
@@ -3,21 +3,22 @@ import subprocess
 import os
 import shutil
 import yaml
+import sys
 
 def get_list(divisions):
 
     # Create map of elements of elements of divisions and combine completely into each other for one list
     data_list = []
-    for i, item in enumerate(divisions):
+    for i, key in enumerate(divisions):
         if i==0:
-                for el in item["items"]:
-                    data_list.append({item["name"]: el})
+                for el in divisions[key]:
+                    data_list.append({key: el})
         else:
             data_list_new = []
             for d in data_list:
-                for el in item["items"]:
+                for el in divisions[key]:
                     data_list_new.append(d.copy())
-                    data_list_new[-1][item["name"]] = el
+                    data_list_new[-1][key] = el
             data_list = data_list_new
 
     return data_list
@@ -33,23 +34,15 @@ def create_jobs(divisions,base_dir,submit_path,yaml_path):
         # Make job directory name and directory
         job_dir = os.path.join(base_dir,"__".join(["_".join([key,str(data_list_i[key])]) for key in data_list_i]))
         data_list_i["outdir"] = os.path.abspath(job_dir) #NOTE: Since dictionary is not copied this should just edit the original entry in data_list.
-        print("DEBUGGING: base_dir = ",base_dir)
-        print("DEBUGGING: job_dir = ",job_dir)
-        print("DEBUGGING: os.path.abspath(job_dir) = ",os.path.abspath(job_dir))
-        print("DEBUGGING: os.path.join(base_dir,job_dir) = ",os.path.join(base_dir,job_dir))
         try:
-            mkdir_result = os.mkdir(job_dir)
-            print("DEBUGGING: mkdir_result = ",mkdir_result)#DEBUGGING
+            os.mkdir(job_dir)
         except FileExistsError:
             print("WARNING: ",job_dir,"already exists.")
 
         # Copy files into job directory
         submit_path_i = os.path.join(job_dir,os.path.basename(submit_path))
         yaml_path_i   = os.path.join(job_dir,os.path.basename(yaml_path))
-        print("DEBUGGING: submit_path = ",submit_path)
-        print("DEBUGGING: submit_path_i = ",submit_path_i)
         cp_result = shutil.copyfile(submit_path,submit_path_i)
-        print("DEBUGGING: cp_result = ",cp_result)
         shutil.copyfile(yaml_path,yaml_path_i)
 
         # Replace key values in yaml file from list and insert if non-existent
@@ -63,8 +56,6 @@ def create_jobs(divisions,base_dir,submit_path,yaml_path):
         # Replace path to yaml with path to job yaml in submit script
         with open(submit_path_i, 'r') as submit_i:
             doc = submit_i.read()
-        print("DEBUGGING: replacing os.path.abspath(yaml_path)   = ",os.path.abspath(yaml_path))
-        print("DEBUGGING: with      os.path.abspath(yaml_path_i) = ",os.path.abspath(yaml_path_i))
         doc = doc.replace(os.path.abspath(yaml_path),os.path.abspath(yaml_path_i)) #NOTE: YAML SHOULD SPECIFY THE OUTPUT DIRECTORY FOR THE JOB. COULD HAVE A DEFAULT JOB DIRECTORY TO REPLACE THOUGH...
         with open(submit_path_i, 'w') as submit_i:
             submit_i.write(doc)
@@ -112,13 +103,13 @@ if __name__=="__main__":
     """
 
     # Create job submission structure
-    methods = {"name":"method","items":["HB","LF"]}
-    fitvars = {"name":"fitvar","items":["costheta1","costheta2"]}
-    sgasyms = {"name":"sgasym","items":[-0.1, -0.01, 0.00, 0.01, 0.1]}
-    bgasyms = {"name":"bgasym","items":[-0.1, -0.01, 0.00, 0.01, 0.1]}
-    seeds   = {"name":"inject_seed","items":[2]}
-    # seeds   = {"name":"inject_seed","items":[2**i for i in range(16)]}
-    sgcuts  = {"name":"sgcut","items":["mass_ppim>1.08 && mass_ppim<1.11","mass_ppim>1.15 && mass_ppim<1.18","(mass_ppim>1.08 && mass_ppim<1.11) || (mass_ppim>1.15 && mass_ppim<1.18)"]}
+    methods = {"method":["HB","LF"]}
+    fitvars = {"fitvar":["costheta1","costheta2"]}
+    sgasyms = {"sgasym":[-0.1, -0.01, 0.00, 0.01, 0.1]}
+    bgasyms = {"bgasym":[-0.1, -0.01, 0.00, 0.01, 0.1]}
+    seeds   = {"inject_seed":[2]}
+    # seeds   = {"inject_seed":[2**i for i in range(16)]}
+    sgcuts  = {"sgcut":["mass_ppim>1.08 && mass_ppim<1.11","mass_ppim>1.15 && mass_ppim<1.18","(mass_ppim>1.08 && mass_ppim<1.11) || (mass_ppim>1.15 && mass_ppim<1.18)"]}
 
     #TODO: Add argumeents for outdir and random seed in analysis.cpp
 
@@ -127,10 +118,10 @@ if __name__=="__main__":
     submit_path = base_dir+"submit.sh"
     yaml_path   = base_dir+"args.yaml"
     out_path    = base_dir+"jobs.txt"
-    divisions = [
+    divisions = dict(
         methods,
-        fitvars,
-    ]
+        **fitvars,
+    )
     create_jobs(divisions,base_dir,submit_path,yaml_path)
     submit_jobs(divisions,base_dir,submit_path,out_path)
 
@@ -139,11 +130,11 @@ if __name__=="__main__":
     # submit_path = base_dir+"submit.sh"
     # yaml_path   = base_dir+"args.yaml"
     # out_path    = base_dir+"jobs.txt"
-    # divisions = [
+    # divisions = dict(
     #     methods,
-    #     fitvars,
-    #     sgcuts,
-    # ]
+    #     **fitvars,
+    #     **sgcuts,
+    # )
     # create_jobs(divisions,base_dir,submit_path,yaml_path)
     # submit_jobs(divisions,base_dir,submit_path,out_path)
 
@@ -152,12 +143,12 @@ if __name__=="__main__":
     submit_path = base_dir+"submit.sh"
     yaml_path   = base_dir+"args.yaml"
     out_path    = base_dir+"jobs.txt"
-    divisions = [
+    divisions = dict(
         methods,
-        fitvars,
-        sgasyms,
-        bgasyms,
-        seeds,
-    ]
+        **fitvars,
+        **sgasyms,
+        **bgasyms,
+        **seeds,
+    )
     create_jobs(divisions,base_dir,submit_path,yaml_path)
     submit_jobs(divisions,base_dir,submit_path,out_path)
