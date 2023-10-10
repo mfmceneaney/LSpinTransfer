2c502528c6f78d77ae83772ddf1e511131d25de9
Matthew McEneaney <mfm45@phyadmins-MBP.lan>
2023-10-09 21:04:22 -0400

    Fixed error with systematics not showing up.

--- aggregate_results.py
+++ aggregate_results.py
@@ -342,7 +342,7 @@ def get_plots(
     plt.ylabel(ytitle,usetex=True)
 
     #TODO: DEBUGGING MESSAGE FOR BINS SEE IF SOMETHING GETS MESSED UP THERE AND MAKE SURE YOU ARE SETTING CORRECTLY...
-    s1 = plt.hist([1 for i in range(len(yerr_syst))], weights=yerr_syst, bins=xbins, color='gray', alpha=0.5, label='Systematic Error')
+    s1 = plt.hist(x_mean, weights=yerr_syst, bins=xbins, color='gray', alpha=0.5, label='Systematic Error') #NOTE: THAT HISTOGRAM X DATA SHOULD JUST USE BIN X MEANS SO THAT EACH BIN GETS ONE ENTRY AND THEN YOU CAN SCALE APPROPRIATELY WITH THE WEIGHTS ARGUMENT.
     g2 = plt.errorbar(x_mean,y_mean,xerr=xerr_mean,yerr=yerr_mean,
                         ecolor=ecolor, elinewidth=elinewidth, capsize=capsize,
                         color=color, marker='o', linestyle=linestyle,
