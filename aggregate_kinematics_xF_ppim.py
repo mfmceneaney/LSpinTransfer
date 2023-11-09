2553d0da8b2aa8d2fc6a2e1b56c24880f57adf85
Matthew McEneaney <matthew.mceneaney@duke.edu>
2023-11-09 09:57:23 -0500

    Fixed default input kinematic variables to look at in `aggregate_kinematics_*.py`.

--- aggregate_kinematics_xF_ppim.py
+++ aggregate_kinematics_xF_ppim.py
@@ -437,12 +437,7 @@ if __name__=="__main__":
     # Get list of directories across which to aggregate
     aggregate_keys = []
     var_lims = {
-        'Q2':[1.0,11.0],
-        'W':[2.0,5.0],
-        'x':[0.0,1.0],
-        'xF_ppim':[0.0,1.0],
-        'y':[0.0,0.8],
-        'z_ppim':[0.0,1.0],
+        'xF_ppim':[-1.0,1.0],
     }
     out_file_list = get_out_file_list(divisions,base_dir,submit_path,yaml_path,var_lims,get_out_file_name,use_mc,aggregate_keys)
 
