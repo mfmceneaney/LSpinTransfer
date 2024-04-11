7c917c96c77ea8fd559e8a48f13896dc1291395d
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-04-11 15:00:13 -0400

    Updated y limits of plots in `aggregate_mass_fit_pdiff.py` script.

--- aggregate_mass_fit_pdiff.py
+++ aggregate_mass_fit_pdiff.py
@@ -472,7 +472,7 @@ if __name__=="__main__":
         'y':[0.0,1.0],
         'z_ppim':[0.0,1.1],
     }
-    ylimss = [-0.5,0.5]
+    ylimss = [-0.75,0.75]
     titles = {
         'costheta1':'Spin Transfer along $P_{\Lambda}$',
         'costheta2':'Spin Transfer along $P_{\gamma^{*}}$',
