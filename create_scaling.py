Fixed computation in `create_scaling.py`.

create_scaling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # Set input files whose uncertainties to scale
 infiles = [
@@ -21,10 +22,14 @@ inscalingdir = '/Users/mfm45/drop/results_pippimdihadronbsaanalysis_mc_asym_inje
 inscalingfiles = [os.path.join(inscalingdir,f) for f in inscalingfiles]
 print('inscalingfiles = ',inscalingfiles)
 
+# Set parameters
+pol = 0.85
+asym = 0.2
+
 # Loop files and scale
 skiprows = 1
 delimiter = ','
-scale_factor_index = 2 #NOTE: SHOULD BE BIN,ACCEPTANCERATIO,STATISTICSSCALEFACTOR,...
+scale_factor_index = 3 #NOTE: SHOULD BE BIN,ACCEPTANCERATIO,STATISTICSSCALEFACTOR,RGHSTATISTICS...
 for i, name in enumerate(infiles):
     
     # Load input file
@@ -48,11 +53,14 @@ for i, name in enumerate(infiles):
     print('b       = ',b)
     print('b.shape = ',b.shape)
     b1 = b[:,scale_factor_index]
+    b1 = 1/pol * np.sqrt(1-np.square(pol*asym)) / np.sqrt(b1)
+    print('b1/a1 = ',b1/a1)
+    a1 = b1
     
     # Scale data
-    print('b1 = ',b1)
-    print('scaling = ',1/np.sqrt(b1))
-    a1 /= np.sqrt(b1) #NOTE: UNCERTAINTIES SCALE LIKE 1/SQRT(N)
+    # print('b1 = ',b1)
+    # print('scaling = ',1/np.sqrt(b1))
+    # a1 /= np.sqrt(b1) #NOTE: UNCERTAINTIES SCALE LIKE 1/SQRT(N)
     a[:,1] = a1
     print('a_rescaled = ',a)
     print('a_rescaled.shape = ',a.shape)
@@ -75,5 +83,12 @@ for i, name in enumerate(infiles):
     # Write the file out again
     with open(outname, 'w') as file:
         file.write(filedata)
+
+    plt.figure()
+    plt.ylim(*(0.0,0.3))
+    plt.errorbar(a[:,0],[asym for el in a[:,0]],a[:,1], fmt='r^', linewidth=2, capsize=6)
+    plt.xlabel(acols[0])
+
+plt.show()
 
 print("DONE")