539534f359be67d77711c259957bbdadd7a130e4
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-12-10 14:28:12 -0500

    Updated workspace titles to match names in `include/analysis.h`.

--- include/analysis.h
+++ include/analysis.h
@@ -5808,8 +5808,8 @@ void getKinBinnedGraphGenericDiff(
         double bin_max = bins[i];
 
         // Create workspace
-        RooWorkspace *ws1 = new RooWorkspace(Form("%s_1",workspace_name.c_str()),workspace_title.c_str());
-        RooWorkspace *ws2 = new RooWorkspace(Form("%s_2",workspace_name.c_str()),workspace_title.c_str());//NOTE: Use two workspaces to avoid naming conflicts
+        RooWorkspace *ws1 = new RooWorkspace(Form("%s_1",workspace_name.c_str()),Form("%s_1",workspace_name.c_str()));
+        RooWorkspace *ws2 = new RooWorkspace(Form("%s_2",workspace_name.c_str()),Form("%s_2",workspace_name.c_str()));//NOTE: Use two workspaces to avoid naming conflicts
 
         // Make bin cut on frame
         std::string  bin_cut = Form("(%s>=%.16f && %s<%.16f)",binvar.c_str(),bin_min,binvar.c_str(),bin_max);
