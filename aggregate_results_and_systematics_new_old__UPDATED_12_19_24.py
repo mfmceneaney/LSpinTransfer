1de9a2dcf55f84f92cd63ed77b24c7483abce8e2
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-12-19 13:12:39 -0500

    Updated x offset to constant 0.05 in `aggregate_results_and_systematics_new_old__UPDATED_12_19_24.py`.

--- aggregate_results_and_systematics_new_old__UPDATED_12_19_24.py
+++ aggregate_results_and_systematics_new_old__UPDATED_12_19_24.py
@@ -595,7 +595,7 @@ def get_plots(
     #s1 = plt.hist(x_mean, weights=yerr_syst, bins=xbins, color='gray', alpha=0.5, label='Systematic Error') #NOTE: THAT HISTOGRAM X DATA SHOULD JUST USE BIN X MEANS SO THAT EACH BIN GETS ONE ENTRY AND THEN YOU CAN SCALE APPROPRIATELY WITH THE WEIGHTS ARGUMENT.
     
     # Now plot old results first
-    x_mean_old = x_mean_old+0.1*x_mean_old #NOTE: OFFSET X OF OLD RESULTS
+    x_mean_old = x_mean_old+0.05 #NOTE: OFFSET X OF OLD RESULTS
     g1_old = plt.errorbar(x_mean_old,y_mean_old,xerr=None,yerr=yerr_syst_old,
                 ecolor='gray', elinewidth=elinewidth*20, capsize=0,
                 color=color, marker='o', linestyle=linestyle, alpha=0.5,
