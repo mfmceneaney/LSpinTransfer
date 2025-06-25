fix: Fix asymmetry injection correction to have correct sign and to be a fractional bin by bin correction.

aggregate_results_and_systematics_OVERALL_momc.py
@@ -801,8 +801,9 @@ if __name__=="__main__":
             print("\n\nDEBUGGING: cos_phi_h_ppim_systematic = ",cos_phi_h_ppim_systematic,"\n\n") #TODO: ADD THIS TO THE SYSTEMATIC ERROR ADDITIVE MAT BELOW AND THE SUMMARY PLOTS.
             #----------------------------------------------------------------------------------------------------#
 
-            # # Apply MC corrections
-            # arrs['y_mean'] += y_corrections_mc_asym_injection
+            # Apply MC corrections
+            y_corrections_mc_asym_injection = np.multiply(y_corrections_mc_asym_injection/sgasym,arrs['y_mean'])
+            arrs['y_mean'] -= y_corrections_mc_asym_injection
 
             # Compute systematics
             alpha_lambda_systematic = 0.0120

aggregate_results_and_systematics_momc.py
@@ -839,7 +839,8 @@ if __name__=="__main__":
             #----------------------------------------------------------------------------------------------------#
 
             # Apply MC corrections
-            arrs['y_mean'] += y_corrections_mc_asym_injection
+            y_corrections_mc_asym_injection = np.multiply(y_corrections_mc_asym_injection/sgasym,arrs['y_mean'])
+            arrs['y_mean'] -= y_corrections_mc_asym_injection
 
             # Compute systematics
             alpha_lambda_systematic = 0.0120