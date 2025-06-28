571c03158ba70487edf99d9fffacf159fb0e72e5
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-06-28 17:26:10 -0400

    feat: Add environment scripts.

---
+++ config/env.csh
@@ -0,0 +1,6 @@
+#!/bin/tcsh
+
+# Set environment
+setenv LSPINTRANSFER_HOME "/work/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer/"
+setenv RGA_MC_DIR "/volatile/clas12/users/mfmce/mc_jobs_rga_ppim_momc_5_20_25/"
+setenv RGA_DT_DIR "/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25/"
---
+++ config/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set environment
+export LSPINTRANSFER_HOME="/work/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer/"
+export RGA_MC_DIR="/volatile/clas12/users/mfmce/mc_jobs_rga_ppim_momc_5_20_25/"
+export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25/"
