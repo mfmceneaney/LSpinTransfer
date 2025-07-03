971760b55da5f11980c281600f3857a1fd656620
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-07-03 10:36:37 -0400

    fix: Fix paths in setup script too.

--- config/setup.sh
+++ config/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set environment
-export LSPINTRANSFER_HOME="/work/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer/"
-export RGA_MC_DIR="/work/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE/"
-export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25/"
+export LSPINTRANSFER_HOME="/work/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer"
+export RGA_MC_DIR="/work/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
+export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25"
 
 # Set paths in ROOT scripts for saga
 for file in $LSPINTRANSFER_HOME/config/*.C; do
