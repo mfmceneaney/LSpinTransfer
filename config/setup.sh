a5ce9464303ca3f298884ff2c253b2e570fdd087
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-07-03 10:46:38 -0400

    fix: Fix paths again.

--- config/setup.sh
+++ config/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set environment
-export LSPINTRANSFER_HOME="/work/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer"
-export RGA_MC_DIR="/work/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
+export LSPINTRANSFER_HOME="/w/hallb-scshelf2102/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer"
+export RGA_MC_DIR="/w/hallb-scshelf2102/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
 export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25"
 
 # Set paths in ROOT scripts for saga
