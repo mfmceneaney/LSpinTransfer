52fddf12f5cf84a6f448d4976c84594673efc6b1
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-07-22 12:33:15 -0400

    feat: Add new MC directories to setup and environment scripts.

--- config/setup.sh
+++ config/setup.sh
@@ -3,12 +3,16 @@
 # Set environment
 export LSPINTRANSFER_HOME="/w/hallb-scshelf2102/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer"
 export RGA_MC_DIR="/w/hallb-scshelf2102/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
+export RGA_MC_40nA_DIR="/volatile/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24"
+export RGA_MC_nobkg_DIR="/voltile/clas12/users/mfmce/mc_jobs_rga_nobkg_ppim_2_23_24"
 export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25"
 
 # Set paths in ROOT scripts for saga
 for file in $LSPINTRANSFER_HOME/config/*.C; do
     sed -i.bak "s;/RGA_DT_DIR;$RGA_DT_DIR;g" $file
     sed -i.bak "s;/RGA_MC_DIR;$RGA_MC_DIR;g" $file
+    sed -i.bak "s;/RGA_MC_40nA_DIR;$RGA_MC_40nA_DIR;g" $file
+    sed -i.bak "s;/RGA_MC_nobkg_DIR;$RGA_MC_nobkg_DIR;g" $file
 done
 
 # Set paths in ROOT scripts for saga
