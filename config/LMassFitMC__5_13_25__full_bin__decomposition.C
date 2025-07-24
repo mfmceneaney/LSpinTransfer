a502ea3e68d205ba5b0bd4888f4c760ee5f12eaf
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-07-24 11:28:39 -0400

    fix: Fix histogram MC decomposition conditions.

--- config/LMassFitMC__5_13_25__full_bin__decomposition.C
+++ config/LMassFitMC__5_13_25__full_bin__decomposition.C
@@ -14,14 +14,14 @@ void LMassFitMC__5_13_25__full_bin__decomposition() {
     std::string true_proton_false_pion_cuts = Form("(%s) && !(%s)",protonangcuts.c_str(),pionangcuts.c_str());
     std::string false_proton_true_pion_cuts = Form("!(%s) && (%s)",protonangcuts.c_str(),pionangcuts.c_str());
     std::string angcuts = Form("(%s) && (%s)",protonangcuts.c_str(),pionangcuts.c_str());
-    std::string angorcuts = Form("(%s) || (%s)",protonangcuts.c_str(),pionangcuts.c_str());
+    std::string angornottcuts = Form("!(%s) || !(%s)",protonangcuts.c_str(),pionangcuts.c_str());
     std::string nomultiplicitycut = "!TMath::IsNaN(costheta1_mc) && !TMath::IsNaN(costheta2_mc)";
     std::string cuts_all = "mass_ppim<1.24 && Q2>1 && W>2 && y<0.8 && xF_ppim>0.0 && z_ppim<1.0 && detector_p==6 && detector_pim==6 && sqrt(px_e*px_e+py_e*py_e+pz_e*pz_e)>2.0 && vz_e>-10 && vz_e<2.5";//Q2>=2.663 && Q2<11.0                                                 
     std::string cuts = Form("%s && (%s)",cuts_all.c_str(),nomultiplicitycut.c_str()); //NOTE: ONLY LOOK AT MC EVENTS WHICH ARE EITHER BACKGROUND OR LAMBDA NOT PROTON PION COMBOS FROM MC TRUTH
     std::string mccuts  = Form("(%s) && (ppid_p_mc==3122 && pidx_p_mc==pidx_pim_mc && (%s))",cuts.c_str(),angcuts.c_str()); //NOTE YOU NEED ANGLE CHECKING FOR ALL TRUTH SIGNAL ITEMS SINCE YOU CAN HAVE COMBINATIONS FROM MC THAT WOULDN'T SHOW UP IN DATA SPECIFICALLY FOR TRUE PROTON FALSE PION
-    std::string mccuts_true_proton = Form("(%s) && (ppid_p_mc==3122 && pidx_p_mc==pidx_pim_mc && (%s))",cuts.c_str(),true_proton_false_pion_cuts.c_str());
-    std::string mccuts_true_pion   = Form("(%s) && (ppid_pim_mc==3122 && pidx_p_mc==pidx_pim_mc && (%s))",cuts.c_str(),false_proton_true_pion_cuts.c_str());
-    std::string mccuts_true_bg     = Form("(%s) && (!(ppid_p_mc==3122 && pidx_p_mc==pidx_pim_mc) || !(%s))",cuts.c_str(),angcuts.c_str()); //NOTE: USE ANGCUTS FOR FULL BG TRUTH SPECTRUM. 9/14/23. //NOTE: USE ANGULAR OR CUTS HERE //NOTE: OLD KEEP FOR DEBUGGING
+    std::string mccuts_true_proton = Form("(%s) && ( (ppid_p_mc==3122 && (%s)) && !(pidx_p_mc==pidx_pim_mc && (%s)) )",cuts.c_str(),protonangcuts.c_str(),pionangcuts.c_str());
+    std::string mccuts_true_pion   = Form("(%s) && ( (ppid_pim_mc==3122 && (%s)) && !(pidx_p_mc==pidx_pim_mc && (%s)) )",cuts.c_str(),pionangcuts.c_str(),protonangcuts.c_str());
+    std::string mccuts_true_bg     = Form("(%s) && ( !(ppid_p_mc==3122 && (%s)) && !(ppid_pim_mc==3122 && (%s)) )",cuts.c_str(),protonangcuts.c_str(),pionangcuts.c_str()); //NOTE: USE ANGCUTS FOR FULL BG TRUTH SPECTRUM. 9/14/23. //NOTE: USE ANGULAR OR CUTS HERE //NOTE: OLD KEEP FOR DEBUGGING
 
     std::string tree = "t";
     std::string path = "/RGA_MC_DIR/skim_*.root";
