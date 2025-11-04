af0ffd3b6377cabf49867379142f09191162e4e7
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-11-04 14:58:10 -0500

    fix: Fix plot labels for experiments comparison.

--- config/plot_results_with_hermes_and_compass.py
+++ config/plot_results_with_hermes_and_compass.py
@@ -141,12 +141,7 @@ def get_plots(
     plt.legend(loc='best',frameon=False)
 
     # Plot subplot labels for paper
-    colors = {
-        'costheta1':'blue',
-        'costheta2':'red',
-    }
-    fitvars = {colors[fitvar]:fitvar for fitvar in colors}
-    ax1.text(0.95, 0.15, '(a)' if fitvars[color]=='costheta1' else '(b)', transform=ax1.transAxes,
+    ax1.text(0.95, 0.15, '(a)' if xvar=='z_ppim' else '(b)', transform=ax1.transAxes,
             fontsize=plt.rcParams['axes.titlesize'], fontweight='bold', va='top', ha='right')
 
     print("DEBUGGING: plt.savefig(outpath) -> ",outpath)
