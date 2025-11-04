0f39acfb54ba0d17ccb683a39ba3c5000dde4049
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-11-04 13:37:37 -0500

    fix: Increase fontsize for plot labels.

--- config/aggregate_kinematics_mass_ppim_momc.py
+++ config/aggregate_kinematics_mass_ppim_momc.py
@@ -359,7 +359,7 @@ def get_plots(
     }
     fitvars = {colors[fitvar]:fitvar for fitvar in colors}
     ax1.text(-0.1, 1.05, '(a)' if fitvars[color]=='costheta1' else '(b)', transform=ax1.transAxes,
-            fontweight='bold', va='top', ha='right')
+            fontsize=plt.rcParams['axes.titlesize'], fontweight='bold', va='top', ha='right')
     print("DEBUGGING: plt.savefig(outpath) -> ",outpath)
     f1.savefig(outpath)
 
