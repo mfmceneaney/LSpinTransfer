fix: Updated how systematics for injection are computed to use max diff from average.

config/aggregate_results_and_systematics_momc.py
@@ -801,9 +801,17 @@ if __name__=="__main__":
                 print("DEBUGGING: Loading mc_asym_injection systematics from ",mc_asym_injection_outpath)
                 # yerr_syst_mc_asym_injection.append(load_systematics_from_aggregate_csv(results_dir=base_dir,base_dir='systematics/mc_asym_injection/',outpath=mc_asym_injection_outpath)['yerr'].to_numpy().tolist())
                 y_corrections_mc_asym_injection_syst = load_systematics_from_aggregate_csv(results_dir=base_dir,base_dir='systematics/mc_asym_injection/',outpath=mc_asym_injection_outpath)['y'].to_numpy()
-                yerr_syst_mc_asym_injection.append(np.abs(np.subtract(y_corrections_mc_asym_injection_syst,y_corrections_mc_asym_injection)))
+                # yerr_syst_mc_asym_injection.append(np.abs(np.subtract(y_corrections_mc_asym_injection_syst,y_corrections_mc_asym_injection)))
+                yerr_syst_mc_asym_injection.append(y_corrections_mc_asym_injection_syst)
                 print("DEBUGGING: type(yerr_syst_mc_asym_injection[-1]) = ",type(yerr_syst_mc_asym_injection[-1]))
                 print("DEBUGGING: yerr_syst_mc_asym_injection[-1]       = ",yerr_syst_mc_asym_injection[-1])
+
+            # Now get average and then diffs
+            averages = np.average(yerr_syst_mc_asym_injection,axis=0)
+
+            yerr_syst_mc_asym_injection = [
+                np.abs(np.subtract(el,averages)) for el in yerr_syst_mc_asym_injection
+            ]
 
             # Now get max diff
             yerr_syst_mc_asym_injection = np.max(yerr_syst_mc_asym_injection,axis=0)