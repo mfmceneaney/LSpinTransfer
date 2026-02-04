ba50b4e3d7b14a08c304a8a9500c08933a036bfd
Matthew F. McEneaney <mfmceneaney@gmail.com>
2026-02-03 19:25:11 -0500

    fix: Update env bash script for duke environment.

--- config/env.sh
+++ config/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set environment
-export LSPINTRANSFER_HOME="/w/hallb-scshelf2102/clas12/users/mfmce/LSpinTransfer_1_17_24/LSpinTransfer"
-export RGA_MC_DIR="/w/hallb-scshelf2102/clas12/users/mfmce/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
-export RGA_MC_40nA_DIR="/volatile/clas12/users/mfmce/mc_jobs_rga_40nA_ppim_2_23_24"
-export RGA_MC_nobkg_DIR="/volatile/clas12/users/mfmce/mc_jobs_rga_nobkg_ppim_2_23_24"
-export RGA_DT_DIR="/volatile/clas12/users/mfmce/dt_jobs_rga_ppim_momc_5_20_25"
+export LSPINTRANSFER_HOME="/hpc/dctrl/mfm45/LSpinTransfer_1_17_24/LSpinTransfer"
+export RGA_MC_DIR="/work/mfm45/mc_jobs_rga_ppim_2_23_24__BACKUP_LEGACY_DO_NOT_DELETE"
+export RGA_MC_40nA_DIR="NULL/volatile/clas12/users/mfmce/mc_jobs_rga_40nA_ppim_2_23_24"
+export RGA_MC_nobkg_DIR="NULL/volatile/clas12/users/mfmce/mc_jobs_rga_nobkg_ppim_2_23_24"
+export RGA_DT_DIR="/work/mfm45/dt_jobs_rga_ppim_momc_5_20_25"
