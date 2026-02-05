0e3c239c970ad8f5076b0d680b566781cc1a8399
Matthew F. McEneaney <mfmceneaney@gmail.com>
2026-02-05 11:43:28 -0500

    fix: Fix slurm scripts.

--- config/systematics/bin_migration/GetBinMigration2D__Inverse__5_14_25/bg/submit.sh
+++ config/systematics/bin_migration/GetBinMigration2D__Inverse__5_14_25/bg/submit.sh
@@ -3,11 +3,10 @@
 #SBATCH --job-name=clas12Lambdas
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out
 #SBATCH --error=/farm_out/%u/%x-%j-%N.err
-#SBATCH --partition=gpu
+#SBATCH --partition=production
 #SBATCH --account=clas12
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=2000
-#SBATCH --gres=disk:1000
 #SBATCH --time=12:00:00
 #SBATCH --mail-user=matthew.mceneaney@duke.edu
 
